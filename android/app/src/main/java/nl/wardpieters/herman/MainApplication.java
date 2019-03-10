8dc77c4dc3d3201594e94a4da11eb63ce8373ac6
Ward Pieters <ward@wardpieters.nl>
2019-03-10 02:22:44 +0100

    Fix

--- android/app/src/main/java/nl/wardpieters/herman/MainApplication.java
+++ android/app/src/main/java/nl/wardpieters/herman/MainApplication.java
@@ -3,7 +3,6 @@ package nl.wardpieters.herman;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new AppCenterReactNativePackage(MainApplication.this)
+          new MainReactPackage()
       );
     }
 

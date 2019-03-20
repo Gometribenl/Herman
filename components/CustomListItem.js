3286837a1815be3f9541857b6950a2b6285eebc7
Ward Pieters <ward@wardpieters.nl>
2019-03-20 21:49:06 +0100

    Orderlist update

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,32 +1,39 @@
 import React, {Component} from "react";
-import {DURATION} from "react-native-easy-toast";
+import {Text, View} from 'react-native';
 import {ListItem} from "react-native-elements";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Display from 'react-native-display';
+
 
 export default class CustomListItem extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            hidden: false,
+        }
+
     }
 
     render() {
         return (
-            <ListItem
-                leftAvatar={{
-                    rounded: false,
-                    size: "large",
-                    imageProps: {
-                        resizeMode: "contain",
-                        backgroundColor: "transparent"
-                    },
-                    source: {
-                        uri: "http://localhost/img.png"
-                    }
-                }}
-                title={"Bestelling #" + item.id.toString()}
-                subtitle={"Betaald met: " + item.payment_method.toString()}
-                onPress={() => {
-                    this.refs.toast.show(item.id.toString(), DURATION.LENGTH_SHORT);
-                }}
-            />
+            <View>
+                <ListItem
+                    rightAvatar={<Icon name="arrow-down" size={25} color="#000"/>}
+                    title={this.props.title}
+                    subtitle={this.props.subtitle}
+                    onPress={() => {
+                        this.setState({
+                            hidden: !this.state.hidden
+                        });
+                    }}
+                />
+
+                <Display enable={this.state.hidden}>
+                    <Text>{this.props.hidden_text}</Text>
+                </Display>
+
+            </View>
         )
     }
 }

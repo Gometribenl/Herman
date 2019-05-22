a678e478b060767c578c1cf87dd81b4c6632452a
Ward Pieters <ward@wardpieters.nl>
2019-05-22 13:32:22 +0200

    Added list of products on order page

--- components/OrderList.js
+++ components/OrderList.js
@@ -3,6 +3,8 @@ import {View} from 'react-native';
 import {ListItem} from "react-native-elements";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Display from 'react-native-display';
+import moment from "moment";
+import 'moment/locale/nl'
 
 export default class OrderList extends Component {
     constructor(props) {
@@ -10,6 +12,23 @@ export default class OrderList extends Component {
 
         this.state = {
             hidden: false,
+        };
+    }
+
+    componentDidMount() {
+        this.setState({title: "Bestelling #" + this.props.order.id});
+        moment.locale('nl');
+        let formattedDate = moment(this.props.order.updated_at).format('LL');
+
+
+        if (this.props.order.is_paid) {
+            this.setState({
+                subtitle: "Betaald op " + formattedDate
+            });
+        } else {
+            this.setState({
+                subtitle: "Besteld op " + formattedDate
+            });
         }
     }
 
@@ -20,8 +39,8 @@ export default class OrderList extends Component {
                     rightAvatar={
                         <Icon name="arrow-down" size={25} color="#000"/>
                     }
-                    title={this.props.title}
-                    subtitle={this.props.subtitle}
+                    title={this.state.title}
+                    subtitle={this.state.subtitle}
                     onPress={() => {
                         this.setState({
                             hidden: !this.state.hidden
---
+++ components/OrderProductsList.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react";
+import {FlatList, View} from 'react-native';
+import {ListItem, Text} from "react-native-elements";
+
+export default class OrderProductsList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <FlatList
+                    data={this.props.items}
+                    keyExtractor={item => item.id.toString()}
+                    ItemSeparatorComponent={this.renderSeparator}
+                    renderItem={({item}) => (
+                        <ListItem
+                            leftAvatar={{
+                                overlayContainerStyle: {
+                                    backgroundColor: 'transparent'
+                                },
+                                rounded: false,
+                                size: "medium",
+                                imageProps: {
+                                    resizeMode: "contain"
+                                },
+                                source: {
+                                    uri: item.product.avatar_url
+                                }
+                            }}
+                            title={item.product.name}
+                            subtitle={item.product.price_formatted}
+                            containerStyle={{
+                                backgroundColor: "#f5f5f5",
+                                paddingTop: 10,
+                                paddingBottom: 10
+                            }}
+                        />
+                    )}
+                />
+
+                <Text style={{marginLeft: 10, marginBottom: 5}}>Totaalbedrag: {this.props.totalPrice}</Text>
+            </View>
+        )
+    }
+}

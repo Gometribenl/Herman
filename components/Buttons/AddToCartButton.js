92d9a9da224d7c2fba41be3bbe21b341aa6867e5
Ward Pieters <ward@wardpieters.nl>
2019-05-29 11:07:17 +0200

    Update add to cart button

--- components/Buttons/AddToCartButton.js
+++ components/Buttons/AddToCartButton.js
@@ -1,8 +1,10 @@
 import React, {Component} from "react";
 import {Alert} from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {API} from "../../global";
+import {API, AuthHeaders, token} from "../../global";
 import AsyncStorage from "@react-native-community/async-storage";
+import {Actions} from "react-native-router-flux";
+import axios from 'axios';
 
 export default class AddToCartButton extends Component {
     constructor(props) {
@@ -10,35 +12,31 @@ export default class AddToCartButton extends Component {
     }
 
     addProductToCart() {
-        AsyncStorage.getItem('jwt').then((token) => {
-            if (token !== null) {
-                let URL = API.BASE_URL + "cart/add";
-                fetch(URL, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'User-Agent': API.USER_AGENT,
-                        'Authorization': "Bearer " + token
-                    },
-                    body: JSON.stringify({
-                        product_id: this.props.productId
-                    })
-                })
-                    .then((response) => response.json())
-                    .then((responseData) => {
-                        console.log(responseData);
-                        if (responseData.data.status === true) {
-                            Alert.alert("Product", responseData.data.message);
-                        } else {
-                            Alert.alert("Fout", responseData.data.message);
-                        }
-                    })
-                    .catch(error => {
-                        Alert.alert("Error", error.message);
-                    });
-            }
-        })
+        let URL = API.BASE_URL + "cart/add";
+        console.log("addProductToCart token: " + token);
+        console.log("" + this.props.attributes);
+
+        if (token !== null) {
+            axios.post(URL, {
+                product_id: this.props.productId
+            }, {
+                headers: AuthHeaders(token)
+            }).then((response) => {
+                console.log(response);
+                if (response.data.data.status === true) {
+                    Alert.alert("Product", response.data.data.message);
+                } else {
+                    Alert.alert("Fout", response.data.data.message);
+                }
+            })
+                .catch(error => {
+                    console.log(error.response);
+                    Alert.alert("Error", error.message);
+                });
+        } else {
+            // Token is null so log user out
+            Actions.auth();
+        }
     }
 
     render() {

a58f82d36c52f75cdaa4ee96c7294581619c2b2a
Ward Pieters <ward@wardpieters.nl>
2019-05-29 11:10:57 +0200

    Update pay button

--- components/Buttons/PayButton.js
+++ components/Buttons/PayButton.js
@@ -1,8 +1,10 @@
 import React, {Component} from "react";
 import {Alert, Linking} from "react-native";
-import {API} from "../../global";
+import {API, AuthHeaders, token} from "../../global";
 import AsyncStorage from "@react-native-community/async-storage";
 import {Button} from "react-native-elements";
+import {Actions} from "react-native-router-flux";
+import axios from 'axios';
 
 export default class PayButton extends Component {
     constructor(props) {
@@ -10,33 +12,28 @@ export default class PayButton extends Component {
     }
 
     CheckOutOrder() {
-        AsyncStorage.getItem('jwt').then((token) => {
-            if (token !== null) {
-                let URL = API.BASE_URL + "cart/checkout";
-                fetch(URL, {
-                    method: 'GET',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'User-Agent': API.USER_AGENT,
-                        'Authorization': "Bearer " + token
-                    }
-                })
-                    .then((response) => response.json())
-                    .then((responseData) => {
-                        console.log(responseData);
-                        if (responseData.data.status === true) {
-                            // Open redirect URL in browser
-                            Linking.openURL(responseData.data.payment.redirect_url);
-                        } else {
-                            Alert.alert("Fout", responseData.data.message);
-                        }
-                    })
-                    .catch(error => {
-                        Alert.alert("Error", error.message);
-                    });
-            }
-        })
+        let URL = API.BASE_URL + "cart/checkout";
+        console.log("CheckOutOrder token: " + token);
+
+        if (token !== null) {
+            axios.get(URL, {
+                headers: AuthHeaders(token)
+            }).then((response) => {
+                console.log(response);
+                if (response.data.data.status === true) {
+                    Linking.openURL(response.data.data.payment.redirect_url);
+                } else {
+                    Alert.alert("Fout", response.data.data.message);
+                }
+            })
+                .catch(error => {
+                    console.log(error.response);
+                    Alert.alert("Error", error.message);
+                });
+        } else {
+            // Token is null so log user out
+            Actions.auth();
+        }
     }
 
     render() {

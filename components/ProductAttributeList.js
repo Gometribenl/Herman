ee3979f49cc5c97c97376c459df5e02496b2eda0
Ward Pieters <ward@wardpieters.nl>
2019-07-03 10:19:44 +0200

    Product Attribute List updated Hide price when zero Change icon when attribute will remove something from the order

--- components/ProductAttributeList.js
+++ components/ProductAttributeList.js
@@ -41,11 +41,22 @@ export default class ProductAttributeList extends Component {
             <View style={{flexDirection: 'column'}}>
                 {
                     this.props.attributes.map((item) => {
+                        let checkedIcon = this.props.checkedIcon;
+                        let title = item.name + " - " + item.price_formatted;
+
+                        if (item.name.toLowerCase().includes("zonder")) {
+                            checkedIcon = "minus-square-o";
+                        }
+
+                        console.log(item);
+
+                        if (item.price === 0 || item.price === "0") title = item.name;
+
                         return (
                             <CheckBox
                                 key={item.id}
-                                title={item.name + " - " + item.price_formatted}
-                                checkedIcon={this.props.checkedIcon}
+                                title={title}
+                                checkedIcon={checkedIcon}
                                 uncheckedIcon={this.props.uncheckedIcon}
                                 uncheckedColor={this.props.uncheckedColor}
                                 checked={this.state.checkBoxChecked[item.id]}
